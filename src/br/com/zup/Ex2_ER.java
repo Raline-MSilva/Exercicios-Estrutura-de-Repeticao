03ccf5b1adaa790f7ca7440011f9c8fa7f12fe1d
Raline Silva <ralinesilva1@gmail.com>
2021-08-30 14:28:41 -0300

    resolução do ex.2 estrutura de repetcção

---
+++ src/br/com/zup/Ex2_ER.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Ex2_ER {
+    public static void main(String[] args) {
+        // criando a variavel
+        int contador = 100;
+        // declarando o while
+        while (contador > 0){
+            System.out.println(contador);
+            contador --;
+        }
+    }
+}
