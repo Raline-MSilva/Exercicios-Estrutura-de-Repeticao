495e1015500f36da24d0a8e1fea6da9bd13d3943
Raline Silva <ralinesilva1@gmail.com>
2021-08-30 14:13:59 -0300

    resolução ex.1 estrutura de repetição

---
+++ src/br/com/zup/Ex1_ER.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+
+public class Ex1_ER {
+
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 100){
+            System.out.println(contador);
+            contador ++;
+        }
+
+    }
+}
