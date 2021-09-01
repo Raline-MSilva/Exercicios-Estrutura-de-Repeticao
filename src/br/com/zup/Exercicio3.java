50fb3b15b7b2a69632321fb08c120b9c99dddadd
Raline Silva <ralinesilva1@gmail.com>
2021-09-01 16:53:11 -0300

    ex 3 da lista de revisão resolvido

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -1,4 +1,21 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        // pedindo ao usuario o numero que deseja
+        System.out.println("Olá, digite o número da tabuada que você deseja ver: ");
+        int numeroEscolhido = leitor.nextInt();
+
+        // criando variaveis
+        int contador = 1;
+        //num + " x " + contador + "=" + calculo
+        // iniciando o while
+        while (contador <= 10){
+            System.out.println(numeroEscolhido+ " X " + contador + "=" + numeroEscolhido * contador);
+            contador ++;
+        }
+    }
 }
