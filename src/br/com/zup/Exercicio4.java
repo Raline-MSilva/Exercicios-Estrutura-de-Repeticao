1feeac4d968003f6d3206b8378a4412f8bcb8628
Raline Silva <ralinesilva1@gmail.com>
2021-09-01 17:57:32 -0300

    resolução do 4 da lista de revisão

--- src/br/com/zup/Exercicio4.java
+++ src/br/com/zup/Exercicio4.java
@@ -5,8 +5,28 @@ import java.util.Scanner;
 public class Exercicio4 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+//        Os números primos possuem várias aplicações dentro da Computação, por exemplo
+//        na Criptografia. Um número primo é aquele que é divisível apenas por um e
+//        por ele mesmo. Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
 
         System.out.println("Por favor digite um número positivo: ");
-        int
+        int numeroPrimo = leitor.nextInt();
+        int contador = 1;
+        int numeroDivisor = 0;
+
+        while (contador <= numeroPrimo) {
+
+            if (numeroPrimo % contador == 0) {
+                numeroDivisor++;
+            }
+            contador++;
+        }
+        if (numeroDivisor == 2){
+            System.out.println("O número digitado é primo.");
+        }
+        else {
+            System.out.println("O número digitado não é primo.");
+        }
+
     }
 }
