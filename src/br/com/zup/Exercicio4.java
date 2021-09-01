49ec8cd5b2ca55167742d9560920183bc8a09968
Raline Silva <ralinesilva1@gmail.com>
2021-09-01 17:12:36 -0300

    em resolução

---
+++ src/br/com/zup/Exercicio4.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor digite um número positivo: ");
+        int
+    }
+}
