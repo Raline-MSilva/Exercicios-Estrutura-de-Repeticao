2ca3bed7963d820c9c713d9acb8687a4c1bd2a0c
Raline Silva <ralinesilva1@gmail.com>
2021-09-02 14:16:03 -0300

    resolução do ex.8 da lista 6

---
+++ src/br/com/zup/Ex_8_Lista6.java
@@ -0,0 +1,30 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex_8_Lista6 {
+    public static void main(String[] args) {
+        //Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.
+
+        Scanner leitor = new Scanner(System.in);
+        List<Integer> numeros = new ArrayList<>();
+
+        int contador = 1;
+        int numerosTotal = 5;
+        int valorDadoPeloUsuario = 0;
+
+
+        while (contador <= numerosTotal) {
+            System.out.println("Por favor digite um número" + contador + ": ");
+            valorDadoPeloUsuario = leitor.nextInt();
+
+            numeros.add(valorDadoPeloUsuario); // inicio a minha lista com os numeros escolhido pelo usuario
+                contador++;
+
+        }
+        System.out.println(numeros); // printa os números que foi digitado pelo usuário.
+
+    }
+}
